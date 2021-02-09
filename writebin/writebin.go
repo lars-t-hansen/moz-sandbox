90c4d3c73cf4698e4799d6f611953f49351851f1
Lars T Hansen <lhansen@mozilla.com>
2021-02-09 14:30:16 +0100

    More sophisticated parsing

--- writebin/writebin.go
+++ writebin/writebin.go
@@ -5,21 +5,26 @@
  *
  * Description
  *   The `text ...`, when joined by commas, comprises a space-or-comma
- *   separated sequence of decimal numbers representing byte values.  Write
+ *   separated sequence of unsigned numbers representing byte values.  Write
  *   these bytes to the file named by `out-filename`.
  *
  *   The separator is one or more spaces, or a comma followed by zero or more
  *   spaces, where a space is anything in the \s class.
  *
+ *   Numbers can be binary (0b...), octal (0...) or hex (0x...).
+ *
  * Example
- *   $ writebin foo.txt 97 98 99 10
+ *   $ writebin foo.txt 97 98 99 0x64 0b1010
  *   $ cat foo.txt
- *   abc
+ *   abcd
+ *   $
  *
  * Bugs
  *   It should be possible to use "-" as the out-filename.
  *
- *   Hex and octal numbers are not allowed.
+ *   It should be possible to read from stdin, probably this should be
+ *   the default if there are no text ... arguments, as for cat, and
+ *   maybe in this case the output should default to stdout.
  */
 
 package main
@@ -41,7 +46,7 @@ func main() {
   re, err := regexp.Compile(`\s+|,\s*`); try(err)
   bytes := []byte {}
   for _, bs := range re.Split(strings.Join(os.Args[2:],","), -1) {
-    n, err := strconv.Atoi(bs); try(err)
+    n, err := strconv.ParseUint(bs, 0, 8); try(err)
     bytes = append(bytes, byte(n))
   }
   try(ioutil.WriteFile(outFilename, bytes, 0644))
