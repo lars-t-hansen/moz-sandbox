a1a617f3cc3a565ac11767e92b11da0072558657
Lars T Hansen <lhansen@mozilla.com>
2021-02-09 16:45:34 +0100

    Allow - for the output file name

--- writebin/writebin.go
+++ writebin/writebin.go
@@ -6,7 +6,8 @@
  * Description
  *   The `text ...`, when joined by commas, comprises a space-or-comma
  *   separated sequence of unsigned numbers representing byte values.  Write
- *   these bytes to the file named by `out-filename`.
+ *   these bytes to the file named by `out-filename`.  If `out-filename`
+ *   is `-` then the bytes are written to stdout.
  *
  *   The separator is one or more spaces, or a comma followed by zero or more
  *   spaces, where a space is anything in the \s class.
@@ -20,17 +21,14 @@
  *   $
  *
  * Bugs
- *   It should be possible to use "-" as the out-filename.
- *
  *   It should be possible to read from stdin, probably this should be
  *   the default if there are no text ... arguments, as for cat, and
- *   maybe in this case the output should default to stdout.
+ *   maybe in this case the output could default to stdout.
  */
 
 package main
 
 import (
-  "io/ioutil"
   "log"
   "os"
   "regexp"
@@ -43,13 +41,22 @@ func main() {
     log.Panic("Usage: writebin out-filename text ...")
   }
   outFilename := os.Args[1]
+
   re, err := regexp.Compile(`\s+|,\s*`); try(err)
   bytes := []byte {}
   for _, bs := range re.Split(strings.Join(os.Args[2:],","), -1) {
     n, err := strconv.ParseUint(bs, 0, 8); try(err)
     bytes = append(bytes, byte(n))
   }
-  try(ioutil.WriteFile(outFilename, bytes, 0644))
+
+  var outfile *os.File
+  if outFilename == "-" {
+    outfile = os.Stdout
+  } else {
+    outfile, err = os.OpenFile(outFilename, os.O_WRONLY|os.O_CREATE, 0644); try(err)
+    defer outfile.Close()
+  }
+  _, err = outfile.Write(bytes); try(err)
 }
 
 func try(err error) {
