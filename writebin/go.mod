ae49c2f51b94678ececbdd13b0083e001aa40555
Lars T Hansen <lhansen@mozilla.com>
2021-02-24 14:08:14 +0100

    Cleanup

---
+++ writebin/go.mod
@@ -0,0 +1,5 @@
+module writebin
+
+go 1.15
+
+require github.com/lars-t-hansen/scripting v0.0.0-20210222203112-13c0dbd4bb60
--- writebin/writebin.go
+++ writebin/writebin.go
@@ -1,79 +1,83 @@
-/* Create a binary file from textual byte values
- *
- * Synopsis
- *   writebin out-filename text ...
- *   writebin out-filename
- *   writebin
- *
- * Description
- *   The `text ...`, when joined by commas, comprises a space-or-comma
- *   separated sequence of unsigned numbers representing byte values.  Write
- *   these bytes to the file named by `out-filename`.  If `out-filename`
- *   is `-` then the bytes are written to stdout.
- *
- *   If there are no `text ...` arguments then the input is read from stdin.
- *   In this case, if there is no `out-filename` then the output is written
- *   to stdout.
- *
- *   The separator is one or more spaces, or a comma followed by zero or more
- *   spaces, where a space is anything in the \s class.
- *
- *   Numbers can be binary (0b...), octal (0...) or hex (0x...).
- *
- * Example
- *   $ writebin foo.txt 97 98 99 0x64 0b1010
- *   $ cat foo.txt
- *   abcd
- *   $
- */
-
+/* Create a binary file from textual byte values. */
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+
+	S "github.com/lars-t-hansen/scripting"
 )
 
+const helpText = `		
+Create a binary file from textual byte values.
+
+Usage
+   writebin -h
+   writebin [out-filename [text ...]]
+
+Description
+  The "text ..." arguments, when joined by commas, comprises a space-or-comma
+  separated sequence of unsigned numbers representing byte values.  The program
+  writes these bytes to the file named by "out-filename".  If "out-filename" is
+  "-" then the bytes are written to stdout.
+
+  If there are no "text ..." arguments then the input is read from stdin.  In
+  this case, if there is no "out-filename" then the output is written to stdout.
+
+  Numbers can be binary (0b...), octal (0...) or hex (0x...).
+
+Example
+  $ writebin foo.txt 97 98 99 0x64 0b1010
+  $ cat foo.txt
+  abcd
+  $
+`
+
 func main() {
+	help := false
+	flag.BoolVar(&help, "h", false, "Print help")
+	flag.BoolVar(&help, "help", false, "Print help")
+	flag.Parse()
+	if help {
+		fmt.Print(helpText)
+		return
+	}
+
+	var err error
+	args := flag.Args()
 	outFilename := "-"
-	if len(os.Args) > 1 {
-		outFilename = os.Args[1]
+	if len(args) > 0 {
+		outFilename = args[0]
 	}
 
 	var input string
-	if len(os.Args) < 3 {
+	if len(args) < 2 {
 		bytes, err := ioutil.ReadAll(os.Stdin)
-		try(err)
+		S.Try(err)
 		input = string(bytes)
 	} else {
-		input = strings.Join(os.Args[2:], ",")
+		input = strings.Join(args[1:], ",")
 	}
 
-	re, err := regexp.Compile(`\s+|,\s*`)
-	try(err)
+	re := regexp.MustCompile(`\s+|,\s*`)
 	bytes := []byte{}
 	for _, bs := range re.Split(strings.Trim(input, " \t\n\r"), -1) {
 		n, err := strconv.ParseUint(bs, 0, 8)
-		try(err)
+		S.Try(err)
 		bytes = append(bytes, byte(n))
 	}
 
 	outfile := os.Stdout
 	if outFilename != "-" {
-		outfile, err = os.OpenFile(outFilename, os.O_WRONLY|os.O_CREATE, 0644)
-		try(err)
+		outfile, err = os.Create(outFilename)
+		S.Try(err)
 		defer outfile.Close()
 	}
 	_, err = outfile.Write(bytes)
-	try(err)
-}
-
-func try(err error) {
-	if err != nil {
-		log.Panic(err)
-	}
+	S.Try(err)
 }
